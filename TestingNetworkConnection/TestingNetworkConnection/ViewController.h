da2c64f625b62f8a6665086a21662ef88a9dc2b9
bhtran <bhtran00@gmail.com>
2016-04-17 12:02:36 -0400

    added files to project

---
+++ TestingNetworkConnection/TestingNetworkConnection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingNetworkConnection
+//
+//  Created by Bao Tran on 4/17/16.
+//  Copyright © 2016 Bao Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestingNetworkConnection/TestingNetworkConnection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestingNetworkConnection
+//
+//  Created by Bao Tran on 4/17/16.
+//  Copyright © 2016 Bao Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
